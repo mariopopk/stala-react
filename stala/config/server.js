985fb8bec006075f5176a321224865567e078f2c
Mario Popoca <mariopopocab@gmail.com>
2021-07-03 18:36:37 -0600

    make it better

--- stala/config/server.js
+++ stala/config/server.js
@@ -1,9 +1,14 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '0d7cff94d0ac1ffc6821b1e07b036015'),
-    },
-  },
+  host: env("APP_HOST", "0.0.0.0"),
+  port: env.int("NODE_PORT", 1337),
 });
+
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', '0d7cff94d0ac1ffc6821b1e07b036015'),
+//     },
+//   },
+// });
